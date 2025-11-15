1e08e4b7578cf3aa315aa89bddc6749174998d56
Punith Dewangan <media.punith@gmail.com>
2025-11-15 12:15:49 +0530

    Commited using vs code Deleted all the jupyter notebook files and converted to py files

---
+++ Ansi.py
@@ -0,0 +1,10 @@
+# ANSI escape codes for colors and styles
+RESET = "\033[0m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+BOLD = "\033[1m"
+HIGHLIGHT = "\033[7m"
+BLACK = "\033[30m"
+
+print(f"{RED}This is a red message.{RESET}")
+print(f"{BOLD}{GREEN}{BLACK}This is bold and green.{RESET}")
