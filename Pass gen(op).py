3103bb2f63e791b9d1369978a87ad24872fbc05b
Punith Dewangan <media.punith@gmail.com>
2025-11-18 18:23:38 +0530

    Updated the Function With The random choices function to enable any length of password in th function

--- Pass gen(op).py
+++ Pass gen(op).py
@@ -1,57 +1,58 @@
 import random
 import string
-def passgen(length:int , dig:bool = True , spec:bool = True)->None:
+def passgen(length:int , dig:bool = True , spec:bool = True)->str:
     lower_c = string.ascii_lowercase
     upper_c = string.ascii_uppercase
     digit = string.digits
     special_c = string.punctuation
+    temp = lower_c + upper_c
     if(dig and spec):
-        temp = lower_c + upper_c + digit + special_c
+        temp += digit + special_c
     elif(dig):
-        temp = lower_c + upper_c + digit 
+        temp  += digit
     elif(spec):
-        temp = lower_c + upper_c + special_c
+        temp += special_c
     else:
-        temp = lower_c + upper_c
-    try :
-        if(length <= len(temp)):
-            Password = "".join(random.sample(temp , length))
-            print(f'Password :  {Password}')
-    except ValueError:
-        print(f"The max length of password u can have is {len(temp)}")
-        print("Enter Length Again :")
-        passgen(num() , dig , spec)
+        temp
+    Password = ''.join(random.choices(temp , k = length))
+    return Password
+
 def num()->int :
     try :
-        l = int(input("Enter the length of Password :"))
+        l : int = int(input("Enter the length of Password :"))
         return l ;
     except ValueError:
-        print("Enter a valid intege number :")
+        print("Enter a valid integer number :")
         return 0
+
 def isdig()-> bool :
     try :
-        a = int(input("Do u want digits in ur password (0|1) :"))
+        a :int = int(input("Do u want digits in ur password (0|1) :"))
         if a not in (0, 1):
-            raise ValueError("Input muist be 0 or 1 .....")
+            raise ValueError("Input must be 0 or 1 .....")
         return bool(a);
     except ValueError:
         print("Default is true")
-        return bool(1);
+        return True
+
 def isspecc() ->bool :
     try :
-        a = int(input("Do u want special charcters in ur password (0|1) :"))
+        a : int = int(input("Do u want special charcters in ur password (0|1) :"))
         if a not in (0, 1):
             raise ValueError("Input muist be 0 or 1 .....")
         return bool(a);
     except ValueError:
         print("Default is true")
-        return bool(1);
+        return True
+
 def main():
     l = num()
     d = isdig()
     s = isspecc()
     if l> 0 :
-        passgen(l , d ,s)
+        print(f'Password : \n {passgen(l , d ,s)}')
     else :
         print('Enter a valid number greater than zero')
-main()
+
+if __name__ == '__main__':
+    main()
--- Password Generator(all).py
+++ Password Generator(all).py
@@ -1,20 +1,15 @@
 import random
 import string
-def passgen(length:int)->None:
+def passgen(length:int)->str:
     lower_c = string.ascii_lowercase
     upper_c = string.ascii_uppercase
     digit = string.digits
     special_c = string.punctuation
     temp = lower_c + upper_c + digit + special_c
-    # print(len(temp))
-    try :
-        if(length <= len(temp)):
-            Password = "".join(random.choice(temp , length))
-            print(f'Password :  {Password}')
-    except ValueError:
-        print(f"The max length of password u can have is {len(temp)}")
-        print("Enter Length Again :")
-        passgen(num())
+    Password = "".join(random.choices(temp , k = length))
+    print(f'Password :  {Password}')
+    return Password
+
 def num()->int :
     try :
         l = int(input("Enter the length of Password :"))
@@ -22,10 +17,13 @@ def num()->int :
     except ValueError:
         print("Enter a valid integer number :")
         return 0
+
 def main():
     l = num()
     if l> 0 :
         passgen(l)
     else :
         print('Enter a valid number greater than zero')
-main()
+
+if __name__ == '__main__':
+    main()
