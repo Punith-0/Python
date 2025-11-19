d6c5ed0112f2ce0b83ef7246e73bbfc8e64b9ff8
Punith Dewangan <media.punith@gmail.com>
2025-11-19 19:50:36 +0530

    Improved function

--- Requests/PokeApi.py
+++ Requests/PokeApi.py
@@ -9,6 +9,7 @@ def pokemon_data(Pokemon_name : str)  -> dict[str , any]:
     URL = f"{url}pokemon/{Pokemon_name}"
     response = requests.get(URL)
     if (response.status_code == 200):
+        print(f'{an.GREEN}API accessed : {response.headers['Date']} {an.RESET}')
         return response.json()
     else:
         print(f"{an.RED}Error Occurred {response.status_code} {an.RESET}")
--- Requests/Pokemon.py
+++ Requests/Pokemon.py
@@ -1,4 +1,3 @@
-import requests
 import Ansi as an
 import PokeApi as pa
 
@@ -6,12 +5,14 @@ def main() -> None :
     p_name :str = str(input("Enter Pokemon Name: ")).lower().strip()
     p_data = pa.pokemon_data(p_name)
     if p_data :
-        print(f"{an.GREEN}Name : {p_data['name']} {an.RESET}")
-        print(f"{an.GREEN}Height : {p_data['height']}{an.RESET}")
-        print(f"{an.GREEN}Pokedex id : {p_data['id']}{an.RESET}")
-        print(f"{an.GREEN}Type :  {[t['type']['name'] for t in p_data['types']]}{an.RESET}")
+        print(f"{an.BOLD}{an.GREEN}  POKEDEX OPEN  {an.RESET}")
+        print(f"\t{an.GREEN}Name : {p_data['name'].capitalize()} {an.RESET}")
+        print(f"\t{an.GREEN}Height : {p_data['height']}{an.RESET}")
+        print(f"\t{an.GREEN}Pokedex id : {p_data['id']}{an.RESET}")
+        print(f"\t{an.GREEN}Type :  {[t['type']['name'] for t in p_data['types']]}{an.RESET}")
+        print(f"{an.BOLD}{an.GREEN}  POKEDEX CLOSE {an.RESET}")
     else :
-        print(f"{an.RED}No Data Found for the Pokemon :{p_name}{an.RESET}")
+        print(f"{an.RED}No Data Found for the Pokemon : {p_name}{an.RESET}")
 
 if __name__ == '__main__':
     main()
