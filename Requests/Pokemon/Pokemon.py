5914794e1b557d26ae0483d7107c5c0034d53632
Punith Dewangan <media.punith@gmail.com>
2025-11-28 18:10:18 +0530

    PokeAPI updated

--- Requests/Pokemon/Pokemon.py
+++ Requests/Pokemon/Pokemon.py
@@ -1,15 +1,18 @@
-import Ansi as an
 import PokeApi as pa
+import Ansi as an
 
 def main() -> None :
+    po = pa.Pokemon()
+    print()
     p_name :str = str(input("Enter Pokemon Name: ")).lower().strip()
-    p_data = pa.pokemon_data(p_name)
+    p_data = po.pokemon_data("https://pokeapi.co/api/v2/" , p_name)
     if p_data :
         print(f"{an.BOLD}{an.GREEN}  POKEDEX OPEN  {an.RESET}")
         print(f"\t{an.GREEN}Name : {p_data['name'].capitalize()} {an.RESET}")
         print(f"\t{an.GREEN}Height : {p_data['height']}{an.RESET}")
         print(f"\t{an.GREEN}Pokedex id : {p_data['id']}{an.RESET}")
-        print(f"\t{an.GREEN}Type :  {[t['type']['name'] for t in p_data['types']]}{an.RESET}")
+        types = ", ".join([t['type']['name'] for t in p_data['types']])
+        print(f"\t{an.GREEN}Type : {types}{an.RESET}")
         print(f"{an.BOLD}{an.GREEN}  POKEDEX CLOSE {an.RESET}")
     else :
         print(f"{an.RED}No Data Found for the Pokemon : {p_name}{an.RESET}")
